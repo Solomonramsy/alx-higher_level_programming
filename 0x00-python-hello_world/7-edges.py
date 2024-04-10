9fd48aec5a6bc3439082bcb5156770ecc87d8b71
Solomonramsy <solomonramsy@icloud.com>
2024-04-10 05:03:27 -0700

    python

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+word =  "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:1]
+print("first 3 letters {}".format(word_first_3))
+print("first 2 letters {}".format(word_last_2))
+print("middle word {}".format(middle_word))
+@ 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Secu
