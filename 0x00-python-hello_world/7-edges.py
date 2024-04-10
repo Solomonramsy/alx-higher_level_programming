787cae583d5f734d7b81dbe1764f2af4923aab9a
Solomonramsy <solomonramsy@icloud.com>
2024-04-10 05:06:34 -0700

    python

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -6,8 +6,3 @@ middle_word = word[1:1]
 print("first 3 letters {}".format(word_first_3))
 print("first 2 letters {}".format(word_last_2))
 print("middle word {}".format(middle_word))
-@ 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Secu
